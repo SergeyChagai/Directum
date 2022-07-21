5163e092bcd09353a8c0f764a8f455db2abdd0c0
Chagaj Sergey <chagaj42@gmail.com>
2022-07-21 19:39:03 +0400

    Add .gitignore

--- DirectumTestProject/DirectumTestProject/MainActivity.cs
+++ DirectumTestProject/DirectumTestProject/MainActivity.cs
@@ -1,12 +1,15 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Views;
+using Android.Widget;
 using AndroidX.AppCompat.App;
+using static Android.Views.View;
 
 namespace DirectumTestProject
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : AppCompatActivity, IOnClickListener
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -14,12 +17,27 @@ namespace DirectumTestProject
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            var downloaderButton = FindViewById<Button>(Resource.Id.downloaderButton);
+            downloaderButton.Click += DownloaderButton_Click;
         }
+
+        private void DownloaderButton_Click(object sender, System.EventArgs e)
+        {
+            if (sender is Button button)
+            {
+                if (button.Id == Resource.Id.downloaderButton)
+                {
+                    ;
+                }
+            }
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
     }
 }
